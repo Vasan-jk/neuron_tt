083d31ec6da61217ebd0e5b525bf56ae5c309e9b
Vasan j k <114376438+Vasan-jk@users.noreply.github.com>
2025-09-09 08:40:24 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -55,10 +55,8 @@ module tb;
     // DUT signals
     reg        clk;
     reg        rst_n;
-    reg        ena;
     reg  [7:0] ui_in;
     wire [7:0] uo_out;
-    reg  [7:0] uio_in;
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
