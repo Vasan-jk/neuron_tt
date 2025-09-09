8f3d4fc4890e58663129a8ec4f9652fcea63fe7a
Vasan j k <114376438+Vasan-jk@users.noreply.github.com>
2025-09-09 08:46:35 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -55,8 +55,10 @@ module tb;
     // DUT signals
     reg        clk;
     reg        rst_n;
+    reg        ena;
     reg  [7:0] ui_in;
     wire [7:0] uo_out;
+   reg [7:] uio_in;
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
@@ -64,8 +66,10 @@ module tb;
     tt_um_neuron dut (
         .clk(clk),
         .rst_n(rst_n),
+       .ena(ena),
         .ui_in(ui_in),
         .uo_out(uo_out),
+       .uio_in(uio_in),
         .uio_out(uio_out),
         .uio_oe(uio_oe)
     );
